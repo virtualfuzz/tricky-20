6e3d02cc29841255b7d0bf1da745fcf3fdc24a64
Jayden <skewed-fade-deluxe@duck.com>
2025-01-20 11:12:06 +0100

    feat: Add puzzle list at api/v1/puzzles.json

---
+++ src/pages/api/v1/puzzles.json.ts
@@ -0,0 +1,24 @@
+import { getCollection } from "astro:content";
+
+export async function GET({ params, request }) {
+  const { searchParams } = new URL(request.url);
+  const search = searchParams.get("search");
+
+  const puzzlesCollection = await getCollection("puzzles");
+  const puzzleList = puzzlesCollection
+    .filter((puzzle) => {
+      if (search === null || puzzle.id.startsWith(search) === true) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .map((puzzle) => ({
+      id: puzzle.id,
+      you_will_learn: puzzle.data.you_will_learn,
+    }));
+
+  return new Response(JSON.stringify(puzzleList, null, 2), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
