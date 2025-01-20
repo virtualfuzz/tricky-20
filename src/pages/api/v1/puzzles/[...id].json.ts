a9758fc589c34a528b045c089cd25816ea68a005
Jayden <skewed-fade-deluxe@duck.com>
2025-01-20 07:50:22 +0100

    style: Rewrite [...id].json.ts so that is it more clear
    Some variables were named posts, which isn't true, those
    are puzzles.

--- src/pages/api/v1/puzzles/[...id].json.ts
+++ src/pages/api/v1/puzzles/[...id].json.ts
@@ -1,20 +1,18 @@
 import type { APIRoute } from "astro";
 import { getCollection, getEntry } from "astro:content";
 
-const usernames = ["Sarah", "Chris", "Yan", "Elian"];
+export async function getStaticPaths() {
+  const puzzles = await getCollection("puzzles");
+  return puzzles.map((puzzle) => ({
+    params: { id: puzzle.id },
+    props: { puzzle },
+  }));
+}
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const GET: APIRoute = async ({ params }) => {
   const puzzle = await getEntry("puzzles", params.id);
 
   return new Response(JSON.stringify(puzzle.data, null, 2), {
     headers: { "Content-Type": "application/json" },
   });
 };
-
-export async function getStaticPaths() {
-  const posts = await getCollection("puzzles");
-  return posts.map((post) => ({
-    params: { id: post.id },
-    props: { post },
-  }));
-}
