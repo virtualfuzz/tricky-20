3cca8b0145a1407c04635441189b892ff55b2d97
Jayden <skewed-fade-deluxe@duck.com>
2025-01-29 20:32:38 +0100

    feat: Add 404 page for profiles

---
+++ src/pages/api/v1/profiles/[...id].ts
@@ -0,0 +1,38 @@
+import { accounts, db, users } from "../../../../db/schema.ts";
+import { eq } from "drizzle-orm";
+import type { APIContext } from "astro";
+
+export const prerender = false;
+
+export async function GET({ params }: APIContext) {
+  if (params.id === undefined) {
+    throw new Error("params.id is undefined, please report this as a bug!");
+  }
+
+  const user = await db
+    .select({ id: users.id, username: users.name, provider: accounts.provider })
+    .from(users)
+    .leftJoin(accounts, eq(users.id, accounts.userId))
+    .where(eq(users.id, params.id));
+
+  if (user.length <= 0) {
+    return new Response(
+      JSON.stringify(
+        {
+          error:
+            `Profile with ${params.id} doesn't exist, are you sure you wrote it right?`,
+        },
+        null,
+        2,
+      ),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 404,
+      },
+    );
+  }
+
+  return new Response(JSON.stringify(user[0], null, 2), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
