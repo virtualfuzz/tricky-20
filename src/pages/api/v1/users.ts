092fc3ed043a58448aa5932d56272741a428ee48
Jayden <skewed-fade-deluxe@duck.com>
2025-02-08 08:16:34 +0100

    feat: Add ?export=true option for /api/v1/users
    Allows you to "export" the database, also includes solved puzzles
    per user, costly, we don't do it by default.

--- src/pages/api/v1/users.ts
+++ src/pages/api/v1/users.ts
@@ -1,4 +1,4 @@
-import { accounts, db, users } from "../../../db/schema.ts";
+import { accounts, db, solutions, users } from "../../../db/schema.ts";
 import { and, eq, ilike } from "drizzle-orm";
 import type { APIContext } from "astro";
 
@@ -8,6 +8,8 @@ export async function GET({ request }: APIContext) {
   const { searchParams } = new URL(request.url);
   const username = searchParams.get("username");
   const provider = searchParams.get("provider");
+  const needExport = searchParams.get("export");
+  console.log(needExport);
 
   const users_to_show = await db
     .select({
@@ -24,15 +26,41 @@ export async function GET({ request }: APIContext) {
       ),
     );
 
-  const users_hashmap = users_to_show.reduce(
-    (acc: { [key: string]: typeof user & { user_url: string } }, user) => {
-      acc[user.user_id] = {
-        user_url: `/api/v1/users/${user.user_id}`,
-        ...user,
-      };
+  // if export then add the puzzlesSolved from each user (costly so dont do it by default)
+
+  // now only do this
+  // Fetch data for all users asynchronously
+  const usersWithPuzzles = await Promise.all(
+    users_to_show.map(async (user) => {
+      const puzzlesSolved = await db
+        .select({
+          puzzleId: solutions.puzzleId,
+          sha512Solution: solutions.sha512Solution,
+          saltSolution: solutions.saltSolution,
+        })
+        .from(solutions)
+        .where(eq(solutions.userId, user.user_id)); // Use current user's ID
+
+      if (needExport === "true") {
+        return {
+          user_url: `/api/v1/users/${user.user_id}`,
+          ...user,
+
+          puzzlesSolved,
+        };
+      } else {
+        return { user_url: `/api/v1/users/${user.user_id}`, ...user };
+      }
+    }),
+  );
+
+  // Convert the array into a hashmap
+  const users_hashmap = usersWithPuzzles.reduce(
+    (acc, user) => {
+      acc[user.user_id] = user;
       return acc;
     },
-    {},
+    {} as Record<string, (typeof usersWithPuzzles)[number]>,
   );
 
   return new Response(JSON.stringify(users_hashmap, null, 2), {
